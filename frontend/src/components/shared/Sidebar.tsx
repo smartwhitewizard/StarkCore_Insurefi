c6cd9204b4401d0b74599e98b7f7acacac3b5722
okste1234 <okste1234@gmail.com>
2024-07-27 14:25:53 +0100

    if by chance repo still exist

--- frontend/src/components/shared/Sidebar.tsx
+++ frontend/src/components/shared/Sidebar.tsx
@@ -6,17 +6,17 @@ const Sidebar = () => {
   return (
       <div className="border-r-2 border-r-gray-100 min-h-svh">
         <nav className="w-1/6 pt-8">
-            <ul >
+            <ul>
                 {SideLinks.map((link, index) => (
-                <li key={index} className="text-xl font-medium py-3 pl-2 pr-8">
-                    <Link href={link.href} className="cursor-pointer">
+                <li key={index} className="text-xl font-medium py-3 pl-2 pr-8 cursor-pointer w-full" title={link.name}>
+                    <Link href={link.href}>
                     <div>{link.icon}</div>
                     </Link>
                 </li>
                 ))}
             </ul>
         </nav>
-   </div>
+      </div>
   );
 };
 
--- frontend/src/data/Nav.tsx
+++ frontend/src/data/Nav.tsx
@@ -6,27 +6,33 @@ import { ReactNode } from "react";
 type SideTypes = {
   href: string;
   icon: ReactNode;
+  name: string;
 };
 
 export const SideLinks: SideTypes[] = [
   {
     href: "/dashboard",
     icon: <FaHome />,
+    name: "Dashboard"
   },
   {
     href: "/overview",
     icon: <FaList />,
+     name: "Subscription"
   },
   {
     href: "/packages",
     icon: <FaCloud />,
+     name: "Claims"
   },
   {
     href: "/billing",
     icon: <FaDatabase />,
+     name: "Billing"
   },
   {
     href: "/governance",
-    icon: <BsFillPeopleFill />
+    icon: <BsFillPeopleFill />,
+    name: "Governance"
   },
 ];
